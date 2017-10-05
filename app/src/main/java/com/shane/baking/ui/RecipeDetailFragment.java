a5408013c2a6dd17bd96512927272e429f3d2fd9
Shane Richards <shane.richards121@gmail.com>
2017-10-05 10:58:33 -0500

    minor changes

--- app/src/main/java/com/shane/baking/ui/RecipeDetailFragment.java
+++ app/src/main/java/com/shane/baking/ui/RecipeDetailFragment.java
@@ -1,19 +1,25 @@
 package com.shane.baking.ui;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.shane.baking.R;
 import com.shane.baking.adapters.IngredientAdapter;
 import com.shane.baking.adapters.StepAdapter;
+import com.shane.baking.data.RecipeContract;
 import com.shane.baking.models.Ingredient;
 import com.shane.baking.models.Recipe;
 import com.shane.baking.models.Step;
@@ -49,6 +55,12 @@ public class RecipeDetailFragment extends Fragment implements StepAdapter.OnClic
         RecipeDetailFragment fragment = new RecipeDetailFragment();
         fragment.setArguments(arguments);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -99,5 +111,40 @@ public class RecipeDetailFragment extends Fragment implements StepAdapter.OnClic
     public void onDetach() {
         super.onDetach();
         listener = null;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_recipe_detail, menu);
+
+        MenuItem bookmarkItem = menu.findItem(R.id.action_bookmark);
+        setBookmarkIcon(bookmarkItem);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_bookmark:
+                toggleBookmark();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void toggleBookmark() {
+
+    }
+
+    private void setBookmarkIcon(MenuItem bookmarkItem) {
+        Cursor cursor = getContext().getContentResolver().query(RecipeContract.RecipeEntry.buildRecipeUri(recipe.getId()), null, null, null, null);
+
+        int bookmarkIcon = (cursor != null && cursor.getCount() > 0)
+                ? R.drawable.ic_bookmark_white_24dp
+                : R.drawable.ic_bookmark_border_white_24dp;
+
+        bookmarkItem.setIcon(ResourcesCompat.getDrawable(getResources(), bookmarkIcon, null));
+        if (cursor != null) cursor.close();
     }
 }
