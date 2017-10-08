6b5bf0604d565a6c5d301e70b281b22307caafa3
Shane Richards <shane.richards121@gmail.com>
2017-10-07 21:14:40 -0500

    added a test

--- app/src/androidTest/java/com/shane/baking/MainActivityTest.java
+++ app/src/androidTest/java/com/shane/baking/MainActivityTest.java
@@ -3,10 +3,11 @@ package com.shane.baking;
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.shane.baking.ui.MainActivity;
+import com.shane.baking.ui.RecipeDetailActivity;
 
 import org.junit.After;
 import org.junit.Before;
@@ -17,6 +18,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -28,7 +31,7 @@ public class MainActivityTest {
     private IdlingResource idlingResource;
 
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> activityTestRule = new IntentsTestRule<>(MainActivity.class);
 
     @Before
     public void registerIdlingResource() {
@@ -44,7 +47,7 @@ public class MainActivityTest {
     @Test
     public void clickRecipeItem_OpensRecipeDetailActivity() {
         onView(withId(R.id.recycler_recipe)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-//        intended(hasComponent(RecipeDetailActivity.class.getName()));
+        intended(hasComponent(RecipeDetailActivity.class.getName()));
     }
 
     @After
